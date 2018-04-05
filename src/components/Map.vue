6374fa12cdda3e90f03f11497c16566f15ae03a0
valeria <vel.gromova@gmail.com>
2018-04-05 22:10:48 +0200

    Add google maps (add api) + add markers on map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>{{ tims }}</div>
+  <div>
+    <gmap-map
+      class="map__wrapper"
+      :center="center"
+      :zoom="9"
+    >
+      <gmap-marker
+        v-for="m in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center=m.position"
+      ></gmap-marker>
+      <!--<gmap-circle-->
+      <!--v-for="m in markers"-->
+      <!--:center="m.position"-->
+      <!--:clickable="true"-->
+      <!--:radius="1000"-->
+      <!--:options="{-->
+      <!--fillColor: 'red',-->
+      <!--fillOpacity: .2,-->
+      <!--strokeColor: '#FF0000',-->
+      <!--strokeOpacity: 0.8,-->
+      <!--strokeWeight: 2-->
+      <!--}"-->
+      <!--&gt;</gmap-circle>-->
+    </gmap-map>
+  </div>
 </template>
 
 <script>
@@ -11,7 +38,11 @@ export default {
 
   data() {
     return {
-      tims: {},
+      center: {
+       lat: 51.509865,
+       lng: -0.118092,
+      },
+      markers: [],
     };
   },
 
@@ -27,15 +58,31 @@ export default {
     axios
       .get('https://data.tfl.gov.uk/tfl/syndication/feeds/tims_feed.xml')
       .then((response) => {
-        this.tims = converter.xml2json(response.data, {
+        let convertedData = converter.xml2js(response.data, {
           compact: true,
           ignoreDoctype: true,
           ignoreAttributes: true,
           ignoreDeclaration: true,
+        })
+
+        convertedData.Root.Disruptions.Disruption.forEach((disruption) => {
+          let coordinates = disruption.CauseArea.DisplayPoint.Point.coordinatesLL._text
+
+          this.markers.push({
+            position: {
+              lng: parseFloat(coordinates.split(',')[0]),
+              lat: parseFloat(coordinates.split(',')[1]),
+            },
+          });
         });
       });
   },
 };
 </script>
 
-<style></style>
+<style>
+  .map__wrapper {
+    width: 100%;
+    height: 500px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import * as VueGoogleMaps from 'vue2-google-maps';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyB22yeCDGYfQu0RtGKR7vS5I4_wmUT2Qyc',
+    libraries: 'places,drawing,visualization',
+  },
+})
 
 /* eslint-disable no-new */
 new Vue({
